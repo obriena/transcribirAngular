161f984a01bfd56f6382bdf02c2b8ec4fa7a3caa
Aaron OBrien <aaron@flyingspheres.com>
2019-11-21 19:26:58 -0600

    changed urls

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,11 +5,11 @@
 export const environment = {
   production: false,
   name: "Integration",
-  loginService: "http://localhost:9080/TextToSpeech/authenticate",
-  registrationService: "http://localhost:9080/TextToSpeech/register",
-  fileUploadService: "http://localhost:9080/TextToSpeech/fileUpload",
-  mediaService: "http://localhost:9080/TextToSpeech/media/findAllForUser/",
-  playMediaService: "http://localhost:9080/TextToSpeech/media/playFileById/"
+  loginService: "http://localhost:9080/transcribir/authenticate",
+  registrationService: "http://localhost:9080/transcribir/register",
+  fileUploadService: "http://localhost:9080/transcribir/fileUpload",
+  mediaService: "http://localhost:9080/transcribir/media/findAllForUser/",
+  playMediaService: "http://localhost:9080/transcribir/media/playFileById/"
 };
 
 
